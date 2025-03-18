28802c870ff08ac1173ff557dac6324c259538da
Sigurd Blakkestad <sigurdb@stacc.com>
2025-03-18 19:07:07 +0100

    feat: change color on navbar

--- src/components/ui/navigation-menu.jsx
+++ src/components/ui/navigation-menu.jsx
@@ -9,7 +9,7 @@ const NavigationMenu = React.forwardRef(({ className, children, ...props }, ref)
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={cn(
-      "relative z-10 flex flex-1 items-center justify-center sticky inset-x-0 top-0 z-[100] h-16 w-full bg-white/30 backdrop-blur-lg transition-all",
+      "relative z-10 flex flex-1 items-center justify-center sticky inset-x-0 top-0 z-[100] h-16 w-full bg-[#A8C3E3]/50 backdrop-blur-lg transition-all",
       className
     )}
     {...props}>
